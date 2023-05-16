ebe01183c4bf9cd6bd3e076937099c40f6a59b55
Rajat Jain <jain9.rajat@gmail.com>
2023-05-16 15:11:20 +0530

    Add a file 'add_meta.py' which enables to change mp3 metadata for mulitple files in a folder(s).

--- scratch/add_meta.py
+++ scratch/add_meta.py
@@ -5,8 +5,11 @@ import pathlib
 
 from utils import media
 
-FILES_LOC = "/Users/rajatj/Personal Drive/My Drive/Jainism/Gurudev Pravachan/Chhah Dhala"
-ALBUM_ART = "/Users/rajatj/Personal Drive/My Drive/Jainism/images/Pandit Daulat Ram Ji.jpg"
+FILES_LOC = "/Users/rajatj/Personal Drive/My Drive/Jainism/Pravachan/Yugal Ji/Bhaag - 1/19 Tattva Charcha Mumbai"
+ALBUM_ART = "/Users/rajatj/Personal Drive/My Drive/Jainism/images/Yugal Ji.jpg"
+
+# This is for regular Gurudev's Pravachans formats of the type:
+# 010_Guj_G-10-12_K-15-18.mp3
 def scan_folder():
     loc = pathlib.Path(FILES_LOC)
     for file in loc.rglob("*.mp3"):
@@ -22,4 +25,16 @@ def scan_folder():
             "Pandit Daulat Ram", ALBUM_ART,
             track_num=index, title=title)
 
-scan_folder()
+def scan_folder1():
+    loc = pathlib.Path(FILES_LOC)
+    for file in loc.rglob("*.mp3"):
+        fname = os.path.basename(file)
+        index = fname.split("_")[1].split(".")[0]
+        title = "Babuji Yugalji Neelambar Tatva Charcha - %s" % index
+        media.remove_metadata(file)
+        media.add_metadata(
+            file, "Neelambar Tatva Charcha", "Babu Jugal Kishore Jain Yugal",
+            "Babu Jugal Kishore Jain Yugal", ALBUM_ART,
+            track_num=index, title=title)
+
+scan_folder1()
